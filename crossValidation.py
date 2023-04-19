ed92a93440e752f986c3764f0c8d183cfc21c8ca
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2023-04-19 16:24:29 +0300

    Function for plotting

--- crossValidation.py
+++ crossValidation.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 from collections.abc import Callable
+import matplotlib.pyplot as plt
+from preprocessing import data_reformatting
+from naiveBayes import Naive_Bayes_Calculate_Measures
+from logisticRegression import Logistic_Regression_Calculate_Measures
 
 def Cross_Validation(df: pd.DataFrame, 
                      measure_calculation_function: Callable[[pd.DataFrame, pd.DataFrame], dict[str, float]], 
@@ -26,3 +30,44 @@ def Cross_Validation(df: pd.DataFrame,
         start += k_fold
 
     return {"Accuracy": accuracy / np.ceil(size / k_fold), "F1_Score": F1_Score / (np.ceil(size / k_fold) - f1_ignore_count), "Log_Loss": Log_Loss / np.ceil(size / k_fold)}
+
+def Plot_Cross_Validation_Results(df:pd.DataFrame, measure_type):
+    print("Naive Bayes Classifier")
+    
+    print("Cross-Validation with k=5")
+    nb_result_5 = Cross_Validation(df, Naive_Bayes_Calculate_Measures, k_fold=5)
+    print(f"{measure_type}: {nb_result_5[measure_type]}")
+    
+    print("Cross-Validation with k=10")
+    nb_result_10 = Cross_Validation(df, Naive_Bayes_Calculate_Measures, k_fold=10)
+    print(f"{measure_type}: {nb_result_10[measure_type]}")
+    
+    # Logistic Regression Performance
+    data_reformatting(df)
+    print("\nLogistic Regression Classifier")
+    
+    print("Cross-Validation with k=5")
+    lr_result_5 = Cross_Validation(df, Logistic_Regression_Calculate_Measures, k_fold=5)
+    print(f"{measure_type}: {lr_result_5[measure_type]}")
+    
+    print("Cross-Validation with k=10")
+    lr_result_10 = Cross_Validation(df, Logistic_Regression_Calculate_Measures, k_fold=10)
+    print(f"{measure_type}: {lr_result_10[measure_type]}")
+    
+    # Plotting the performance
+    
+    X = ["k=5", "k=10"]
+    
+    naive_bayes = [nb_result_5[measure_type], nb_result_10[measure_type]]
+    logistic_regression = [lr_result_5[measure_type], lr_result_10[measure_type]]
+    
+    X_axis = np.arange(len(X))
+    plt.bar(X_axis - 0.2, naive_bayes, 0.4, label = 'Naive Bayes')
+    plt.bar(X_axis + 0.2, logistic_regression, 0.4, label = 'Logistic Regression')
+    
+    plt.xticks(X_axis, X)
+    plt.xlabel("K Fold")
+    plt.ylabel(measure_type)
+    plt.title(f"{measure_type} Comparison")
+    plt.legend()
+    plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import time
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from preprocessing import data_cleaning, data_reformatting
-from naiveBayes import Naive_Bayes_Calculate_Measures
-from logisticRegression import Logistic_Regression_Calculate_Measures
-from crossValidation import Cross_Validation
+from preprocessing import data_cleaning
+from crossValidation import Plot_Cross_Validation_Results
 
 def main():
     train_df = pd.read_csv('data/loan_sanction_train.csv')
@@ -20,53 +17,7 @@ def main():
     """ for feature in non_numeric_features:
         print(f'Unique values of {feature}: {train_df[feature].unique()}') """
 
-    # Naive Bayes Performance
-    print("Naive Bayes Classifier")
-    
-    print("Cross-Validation with k=1")
-    nb_result_1 = Cross_Validation(train_df, Naive_Bayes_Calculate_Measures, k_fold=1)
-    print(nb_result_1)
-    
-    print("Cross-Validation with k=5")
-    nb_result_5 = Cross_Validation(train_df, Naive_Bayes_Calculate_Measures, k_fold=5)
-    print(nb_result_5)
-    
-    print("Cross-Validation with k=10")
-    nb_result_10 = Cross_Validation(train_df, Naive_Bayes_Calculate_Measures, k_fold=10)
-    print(nb_result_10)
-    
-    # Logistic Regression Performance
-    data_reformatting(train_df)
-    print("\nLogistic Regression Classifier")
-    
-    print("Cross-Validation with k=1")
-    lr_result_1 = Cross_Validation(train_df, Logistic_Regression_Calculate_Measures, k_fold=1)
-    print(lr_result_1)
-    
-    print("Cross-Validation with k=5")
-    lr_result_5 = Cross_Validation(train_df, Logistic_Regression_Calculate_Measures, k_fold=5)
-    print(lr_result_5)
-    
-    print("Cross-Validation with k=10")
-    lr_result_10 = Cross_Validation(train_df, Logistic_Regression_Calculate_Measures, k_fold=10)
-    print(lr_result_10)
-    
-    # Plotting the performance
-    
-    X = ["k=1", "k=5", "k=10"]
-    naive_bayes = [nb_result_1["F1_Score"], nb_result_5["F1_Score"], nb_result_10["F1_Score"]]
-    logistic_regression = [lr_result_1["F1_Score"], lr_result_5["F1_Score"], lr_result_10["F1_Score"]]
-    
-    X_axis = np.arange(len(X))
-    plt.bar(X_axis - 0.2, naive_bayes, 0.4, label = 'Naive Bayes')
-    plt.bar(X_axis + 0.2, logistic_regression, 0.4, label = 'Logistic Regression')
-    
-    plt.xticks(X_axis, X)
-    plt.xlabel("K Fold")
-    plt.ylabel("F1_Score")
-    plt.title("F1_Score Comparison")
-    plt.legend()
-    plt.show()
+    Plot_Cross_Validation_Results(train_df, "Accuracy")
 
 if __name__ == '__main__':
     main()
